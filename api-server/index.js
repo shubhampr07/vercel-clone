ba24bd9a4c1ac2605cb40ad8d31885ec9014379d
Shubham Kumar Mandal <shubhampaul756@gmail.com>
2024-04-07 13:34:29 +0530

    first commit

---
+++ api-server/index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const { generateSlug } = require('random-word-slugs')
+const { ECSClient, RunTaskCommand } = require('@aws-sdk/client-ecs')
+const Redis = require('ioredis')
+const cors = require('cors');
+const {Server} = require('socket.io')
+
+
+const app = express();
+
+const PORT = 9000;
+
+app.use(cors());
+
+const subscriber = new Redis(process.env.REDIS_SUBSCRIBER_STRING)
+
+const io = new Server({ cors: '*' })
+
+io.on('connection', socket => {
+    socket.on('subscribe', channel => {
+        socket.join(channel)
+        socket.emit('message', `Joined ${channel}`)
+    })
+})
+
+io.listen(9001, () => {
+    console.log(`Socket IO server is running on 9001`)
+})
+
+const ecsClient = new ECSClient({
+    region: process.env.LOCATION,
+    credentials: {
+        accessKeyId: process.env.ACCESSKEYID,
+        secretAccessKey: process.env.SECRETACCESSKEY,
+    }
+})
+
+const config = {
+    CLUSTER: 'arn:aws:ecs:ap-south-1:562552893446:cluster/builder-cluster',
+    TASK: 'arn:aws:ecs:ap-south-1:562552893446:task-definition/builder-task1'
+}
+
+app.use(express.json())
+
+app.post('/project', async (req, res) => {
+    const { gitURL, slug } = req.body
+    const projectSlug = slug ? slug : generateSlug();
+
+    //spining the docker container
+
+    const command = new RunTaskCommand({
+        cluster: config.CLUSTER,
+        taskDefinition: config.TASK,
+        launchType: 'FARGATE',
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                assignPublicIp: 'ENABLED',
+                subnets: ['subnet-06e3f4966ba74f9e0', 'subnet-05e7c7a02c7a1eeba', 'subnet-09cf26188e138579b'],
+                securityGroups: ['sg-0a193db9c0a4f3f0e']
+            }
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: 'builder-image1',
+                    environment: [
+                        {name: 'GIT_REPOSITORY__URL', value: gitURL},
+                        {name: 'PROJECT_ID', value: projectSlug}
+                    ]
+                }
+            ]
+        }
+    })
+    await ecsClient.send(command);
+    return res.json({ status: 'queued', data: { projectSlug, url: `http://${projectSlug}.localhost:8000`}})
+})
+
+
+async function initRedisSubscribe() {
+    console.log("Subscribed to logs...")
+    subscriber.psubscribe('logs:*')
+    subscriber.on('pmessage', (pattern, channel, message) => {
+        io.to(channel).emit('message', message)
+    })
+}
+
+initRedisSubscribe();
+
+app.listen(PORT, () => {
+    console.log(`API-Server is running on port ${PORT}`);
+})
---
+++ build-server/script.js
@@ -0,0 +1,75 @@
+const { exec } = require('child_process');
+const path = require("path")
+const fs = require("fs")
+const {S3Client, PutObjectCommand} =  require("@aws-sdk/client-s3")
+const mime = require("mime-types")
+const Redis = require('ioredis')
+
+
+const publisher = new Redis(process.env.REDIS_PUBLISHER_STRING)
+
+
+const s3Client = new S3Client({
+    region: process.env.LOCATION,
+    credentials: {
+        accessKeyId: process.env.ACCESSKEYID,
+        secretAccessKey: process.env.SECRETACCESSKEY,
+    }
+})
+
+const PROJECT_ID = process.env.PROJECT_ID;
+
+function publishLog(log) {
+    publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }))
+}
+
+async function init () {
+    console.log('Executingg script.js');
+    publishLog('Build Started...')
+    const outDirPath = path.join(__dirname, 'output')
+
+    const p = exec(`cd ${outDirPath} && npm install && npm run build`)
+
+    p.stdout.on('data', function (data) {
+        console.log(data.toString());
+        publishLog(data.toString())
+    })
+
+    p.stdout.on('error', function (data) {
+        console.log('Error', data.toString());
+        publishLog(`error: ${data.toString()}`)
+    })
+
+    p.on('close', async function () {
+        console.log('Build Complete')
+        publishLog('Build Complete')
+        const distFolderPath = path.join(__dirname, 'output', 'dist')
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true })
+
+        publishLog(`Starting to upload`)
+        for (const file of distFolderContents) {
+            const filePath = path.join(distFolderPath, file);
+            if(fs.lstatSync(filePath).isDirectory()) continue;
+
+            console.log("Uploading", filePath)
+            publishLog(`uploading ${file}`)
+
+            const contentType = mime.lookup(filePath) || 'application/octet-stream';
+
+            const command = new PutObjectCommand({
+                Bucket: 'vercel-clonee',
+                Key: `__outputs/${PROJECT_ID}/${file}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: contentType
+            })
+
+            await s3Client.send(command)
+            publishLog(`Uploaded ${file}`)
+            console.log("Uploaded", filePath)
+        }
+        publishLog(`Your Project Deployed Successfully...`)
+        console.log("Your Project Deployed Successfully...")
+    })
+}
+
+init()
